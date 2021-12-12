b6a1821d74af6c5cfca7726f82cd31cf139ec7b8
miguel-aoyama <aoyamarikiya@gmail.com>
2021-12-12 20:09:49 +0900

    file change

--- index.php
+++ index.php
@@ -3,8 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>my-portfolio</title>
+    <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
-    
+    <header class="page-header">
+      <nav>
+        <ul class="main-nav">
+          <li><a href="about.html">ABOUT</li>
+          <li><a href="#">WORKS</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+
+    </main>
   </body>
 </html>
