5a96774df40a4dba48726349e1fb2057f4a5578e
miguel-aoyama <aoyamarikiya@gmail.com>
2021-12-13 18:21:15 +0900

    add files

--- index.php
+++ index.php
@@ -2,19 +2,50 @@
 <html lang="ja" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>my-portfolio</title>
+    <title>RIKIYA AOYAMA</title>
     <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
-    <header class="page-header">
+    <header class="page-header wrapper">
       <nav>
         <ul class="main-nav">
-          <li><a href="about.html">ABOUT</li>
-          <li><a href="#">WORKS</li>
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="about.html">ABOUT</a></li>
+          <li><a href="#">WORKS</a></li>
         </ul>
       </nav>
     </header>
     <main>
+      <div class="home-content wrapper">
+        <h2 class="page-title">RIKIYA AOYAMA</h2>
+        <p>簡単な自己紹介</p>
+        <div class="works-container">
+          <div class="whatab">
+            <h3><a href="whatab.php">Whatab</a></h3>
+
+            <p>ギターのコードを調べるアプリです。押さえている弦と位置をそのまま入力すると何のコードか検索できます。
+            ピアノの鍵盤の部分に選択中の音が表示されます。</p>
+          </div>
+          <div class="re-cipe">
+            <h3>Re-cipe</h3>
+
+            <p>レシピを作りたい分量に変換するアプリです。料理サイトのレシピをコピペして、何人分にしたいか選択
+            するだけで、分量を知ることができます。</p>
+          </div>
+          <div class="analog-helper">
+            <h3>Analog-Helper</h3>
+
+            <p>アナログシンセサイザーという楽器の設定を保存しておくアプリです。ツリー式になっているので、
+            曲ごとなど、カテゴリを分けておくことができます。</p>
+          </div>
+          <div class="portfolio">
+            <h3>Portfolio Site</h3>
+
+            <p>このサイトです。アプリの魅力を伝えられるようにスクロールアニメを使用しています。</p>
+
+          </div>
+        </div>
+      </div>
 
     </main>
   </body>
---
+++ whatab.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>RIKIYA AOYAMA</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <header class="page-header wrapper">
+      <nav>
+        <ul class="main-nav">
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="about.html">ABOUT</a></li>
+          <li><a href="#">WORKS</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <div class="works-content wrapper">
+        <div class="page-title">
+          <h2>Whatab</h2>
+        </div>
+        <div class="introductions">
+          <div class="PC-main">
+            <p>PC メインページ</p>
+            <div class="PC-main-image">
+              <img src="images/Whatab/pc-main.jpeg" alt="PCメインページ" >
+            </div>
+          </div>
+          <div class="pages">
+            <!--img-->
+          </div>
+        </div>
+        <div class="summry">
+          <h3>プログラムの概要</h3>
+          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+        <div class="purpose">
+          <h3>制作の目的</h3>
+          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+        <div class="sticking">
+          <h3>こだわりのポイント</h3>
+          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+        <div class="langage">
+          <h3>使用言語・開発環境</h3>
+          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
